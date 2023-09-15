5731fbb2d10e4ece246fce3ad173c874a777643d
Daniel <Daniel.falklimberg@gmail.com>
2023-09-15 18:23:25 +0200

    progress maybe

--- src/main.mjs
+++ src/main.mjs
@@ -1,19 +1,26 @@
 import * as PIXI from "pixi.js";
-
 console.log("pixi version:", PIXI.VERSION);
 
 const gameSettings = {
     width: 160,
-    height: 144,
-}
+    height: 144
+};
 
 const app = new PIXI.Application({ 
     width: gameSettings.width, 
     height: gameSettings.height,
-    resolution: window.innerHeight*0.6/gameSettings.height
+    resolution: window.innerHeight*0.6/gameSettings.height,
+    backgroundColor: 0xffffff
 });
 
 document.body.appendChild(app.view);
 
+const font = new FontFaceObserver("gameboyfont", {});
+await font.load();
 
+const text = new PIXI.Text("Hello", {
+    fontFamily: "gameboyfont",
+    fontSize: 8
+});
 
+app.stage.addChild(text);
